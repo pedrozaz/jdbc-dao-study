6611dec5f69139758527ac83ab7c7b9886036efd
Guilherme <guilhermebarb0sa@proton.me>
2025-05-12 21:12:51 -0300

    DaoFactory created

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,7 @@
 package app;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +11,10 @@ public class Program {
     public static void main(String[] args) {
 
         Department dep = new Department(1, "Books");
-        System.out.println(dep);
 
         Seller sel = new Seller(1,"Marcos","marcos@email.com",new Date(), 3000.0, dep);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(sel);
     }
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller o) {
+
+    }
+
+    @Override
+    public void update(Seller o) {
+
+    }
+
+    @Override
+    public void deleteById(Seller id) {
+
+    }
+
+    @Override
+    public Seller findById(Seller id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
